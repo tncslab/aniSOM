86193e45c6e45e554f6404acc1d195a830befdd6
phrenico <benko.zsigmond@gmail.com>
2025-06-06 11:12:51 +0200

    cleaning main branch from codes of other methods.

--- scripts/config.py
+++
@@ -1,9 +0,0 @@
-# Configuration file for the AniSOM project
-from pathlib import Path
-import os
-
-# get username from os
-user = os.environ['USER']
-
-# project path
-project_path = Path(f'/home/{user}/Projects/Codes/aniSOM')  # server
